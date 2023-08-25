0660bf2171e876b5f5b6f0d07290a8ada5b84f91
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-08-25 13:59:48 +0300

    add start markup

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+
+export class Feedback extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+
+export class Statistics extends Component {
+  state = {};
+  render() {
+    return <div></div>;
+  }
+}
